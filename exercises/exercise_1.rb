e842f13adce39034d3df34a8d2e07092db881fe5
yuhaoyann <yuhaoyann@gmail.com>
2022-01-08 23:28:37 -0500

    delete comment

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,7 +3,6 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
 class Stores < ActiveRecord::Base
 end
 burnaby = Stores.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
