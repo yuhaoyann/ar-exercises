d85b1a4df8cea70564845b5b77bdd8c0a78361dd
yuhaoyann <yuhaoyann@gmail.com>
2022-01-08 23:45:08 -0500

    finish exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,8 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+@store3 = Stores.find_by(id: 3)
+store3 = Stores.find_by(id: 3)
+store3.destroy
+puts Stores.count
+
